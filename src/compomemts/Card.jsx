6664cb386628bf2eccec7b36cb494143c179ede5
Sahil Khadka <sahilkhadka767@gmail.com>
2022-01-23 17:25:35 +0545

    more props

--- src/compomemts/Card.jsx
+++ src/compomemts/Card.jsx
@@ -1,20 +1,20 @@
 import React from "react"
+import Avatar from "./Avatar"
 
 function Card (props) {
     return (
         <div className="card">
-        <div className="top">
-          <h2 className="name">{props.name}</h2>
-          <img
-            src= {props.img}
-            alt="avatar_img" className="circle-img"
-          />
+            <div className="top">
+                <h2 className="name">{props.name}</h2>
+                <Avatar
+                    img = {props.img}
+                />
+            </div>
+            <div className="bottom">
+                <p className="info">{props.phone}</p>
+                <p className="info">{props.email}</p>
+            </div>
         </div>
-        <div className="bottom">
-          <p className="info">{props.phone}</p>
-          <p className="info">{props.email}</p>
-        </div>
-      </div>
     )
 }
 
