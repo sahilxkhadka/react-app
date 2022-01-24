ff0d8854455eb96759b28d86f0b632f50b2e3b6a
Sahil Khadka <sahilkhadka767@gmail.com>
2022-01-24 18:09:39 +0545

    complete

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -4,42 +4,42 @@ const emojipedia = [
       emojiUrl: "https://img.icons8.com/color/344/html-5--v1.png",
       name: "HTML",
       meaning:
-        "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+      "The HyperText Markup Language, or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript." 
     },
     {
       id: 2,
       emojiUrl: "https://img.icons8.com/color/344/css3.png",
       name: "CSS",
       meaning:
-        "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+        "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript."
     },
     {
       id: 3,
       emojiUrl: "https://img.icons8.com/dusk/344/javascript-logo.png",
       name: "JAVASCRIPT",
       meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        "JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries."
     },
     {
       id: 4,
       emojiUrl: "https://img.icons8.com/color/344/sass-avatar.png",
       name: "SASS",
       meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        "Sass is a preprocessor scripting language that is interpreted or compiled into Cascading Style Sheets. SassScript is the scripting language itself. Sass consists of two syntaxes. The original syntax, called 'the indented syntax,' uses a syntax similar to Haml."
     },
     {
       id: 5,
       emojiUrl: "https://img.icons8.com/officel/344/react.png",
       name: "REACT.JS",
       meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        "React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies. React can be used as a base in the development of single-page or mobile applications"
     },
     {
       id: 6,
       emojiUrl: "https://cdn.worldvectorlogo.com/logos/next-js.svg",
       name: "NEXT.JS",
       meaning:
-        "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+        "Next.js is an open-source development framework built on top of Node.js enabling React based web applications functionalities such as server-side rendering and generating static websites."
     }
   ];
 export default emojipedia  
